ad89c2b75a3f6c45a568ac0dbd2f6a89e73902de
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-05-02 17:20:33 +0100

    localhost works lol

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,8 +1,58 @@
 
+if ($(window).width() < 960) {
+
+  document.getElementsByClassName('logo')[0].style.visibility = 'hidden';
+  document.getElementById('navbar').style.display = 'block';
+
+  document.getElementById('footer').style.display = 'none';
+
+  document.getElementById('h1').style.display = 'none';
+  document.getElementById('rmv').style.display = 'none';
+
+  //document.getElementById('sub-text').style["background-color"] = "red";
+
+  document.getElementById('sub-text').style["position"] = "absolute";
+
+  document.getElementById('sub-text').style["top"] = "20%";
+  document.getElementById('sub-text').style["color"] = "white";
+  document.getElementById('sub-text').style["font-family"] = "Proxima Nova !important";
+
+  document.getElementById('sub-text').style["left"] = "1%";
+  document.getElementById('sub-text').style["height"] = "80%";
+  document.getElementById('sub-text').style["width"] = "98%";
+
+
+
+  document.getElementById('input1').style["width"] = "100%";
+  document.getElementById('input1').style["height"] = "30px";
+  document.getElementById('input1').style["background-color"] = "rgba(0,0,150,1)";
+  document.getElementById('input1').style["color"] = "white";
+
+  document.getElementById('input2').style["width"] = "100%";
+  document.getElementById('input2').style["height"] = "30px";
+  document.getElementById('input2').style["background-color"] = "rgba(0,0,150,1)";
+  document.getElementById('input2').style["color"] = "white";
+
+  document.getElementById('input3').style["width"] = "100%";
+  document.getElementById('input3').style["height"] = "30px";
+  document.getElementById('input3').style["background-color"] = "rgba(0,0,150,1)";
+  document.getElementById('input3').style["color"] = "white";
+
+  document.getElementById('regist').style["width"] = "100%";
+  document.getElementById('regist').style["background-color"] = "rgba(0,0,150,1)";
+
+  document.getElementById('regist').style["height"] = "30px";
+  document.getElementById('regist').style["color"] = "white";
+
+} else {
+
+}
+
+
 socket = io.connect(window.location.origin);
 window.addEventListener("mousedown", myScript);
 
-
+console.log("page loads");
 
 
 $(function() {
@@ -78,6 +128,9 @@ $(document).ready(function() {
 
 
 function register(v) {
+
+console.log("register works");
+
   var reg1 = "2, " + document.getElementById('input1').value + ";";
   var reg2 = "4, " + document.getElementById('input2').value + ";";
   var reg3 = "6, " + document.getElementById('input3').value + ";";
